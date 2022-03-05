2f781bb1c9dae03e1ff051724694babdae66641e
appearedherosheep <81283189+appearedherosheep@users.noreply.github.com>
2022-03-05 23:48:43 +0900

    feat : Add navbar, Searchbox

---
+++ src/components/Navbar.js
@@ -0,0 +1,14 @@
+import React from "react";
+
+
+function Navbar() {
+    return (
+        <div>
+            <button>청운</button>
+            <button>사름</button>
+            <button>Login</button>
+        </div>
+    );
+}
+
+export default Navbar;
---
+++ src/components/Searchbox.js
@@ -0,0 +1,13 @@
+import React from "react";
+
+
+function Search() {
+    return (
+        <div>
+            <input type="text" placeholder="노래 제목/가수를 입력하세요"/>
+            <button>검색</button>
+        </div>
+    );
+}
+
+export default Search;
